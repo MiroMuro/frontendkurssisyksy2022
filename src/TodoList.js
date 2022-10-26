0d66db7215b8641a224cd67848f50b07f68596e6
unknown <miro.laukkanen44@gmail.com>
2022-10-26 21:15:56 +0300

    Date picker

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,8 @@
 import React, { useState, useRef } from 'react';
 import { AgGridReact} from 'ag-grid-react'
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button'
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-material.css'
 
@@ -9,14 +12,18 @@ function Todolist() {
   const gridRef = useRef();
   const columns = [
     { field: "description", sortable: true, filter: true, headerName: "Kuvaus" },
-    { field: "date",    sortable: true, filter: true, headerName: "Päivämäärä" },
+    { field: "date",    sortable: true, filter: true, headerName: "Pvm (YY/MM/DD)" },
     { field: "priority", sortable: true, filter: true, headerName: "Prioriteetti",
-      cellStyle: params => params.value === "High"? {color: 'red'}:{color: 'green'}}    ]
+      cellStyle: params => params.value === "High"? {color: 'red'}:{color: 'green'}},
+      ]
 
   const inputChanged = (event) => {
     setTodo({...todo, [event.target.name]: event.target.value});
   }
-
+  const dateChanged = (date) =>{
+    const [year, month, day] = date.target.value.split('-')
+    todo.date = ((day)+'.'+(month)+'.'+(year))
+  }
   const addTodo = (event) => {
     setTodos([...todos, todo]);
     setTodo({...todo, description:'',date: '',priority: ''})
@@ -31,13 +38,36 @@ function Todolist() {
   }}
 
   return (
-    <div className="ag-theme-material"
-        style={{height: '700px', width: '70%', margin: 'auto'}}>
-        <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description}/>
-      <input type="text" onChange={inputChanged} placeholder="Date" name="date" value={todo.date}/>
-      <input type="text" onChange={inputChanged} placeholder="Priority" name="priority" value={todo.priority}/>
-      <button onClick={addTodo}>Add</button>
-      <button onClick={deleteTodo}>delete</button>
+    
+    <div class="ex1">
+      <Stack direction="row" spacing={2} justifyContent="center" alignitems="center">
+      <TextField
+        label="Description"
+        variant="outlined"
+        name="description"
+        value={todo.description}
+        onChange={inputChanged} />
+      <TextField
+        label="Priority"
+        variant="outlined"
+        name="priority"
+        value={todo.priority}
+        onChange={inputChanged}/>
+      <TextField
+        id="date"
+        label="Date"
+        type="date"
+        onChange={dateChanged}
+        sx={{ width: 220 }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <Button onClick={addTodo} variant="contained" >Add</Button>
+      <Button onClick={deleteTodo}variant="contained" color="error">Delete</Button>
+      </Stack>
+      <div className="ag-theme-material"
+        style={{height: '700px', width: '40%', margin: 'auto'}}>
         <AgGridReact
         ref={gridRef}
         onGridReady={params => gridRef.current = params.api}
@@ -45,6 +75,7 @@ function Todolist() {
         columnDefs={columns}
         rowData={todos}>
         </AgGridReact>
+    </div>
     </div>
   );
 };
